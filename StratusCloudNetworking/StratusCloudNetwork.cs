611599ac0e16738d484dbe180d12691d57da55cd
NormoyleAdam <contact@adamnormoyle.co.uk>
2019-11-12 00:33:43 +0000

    Initial Project Upload

---
+++ StratusCloudNetworking/StratusCloudNetwork.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace StratusCloudNetworking
+{
+    public static class StratusCloudNetwork
+    {
+        public const string masterServer = "masterserver.net";
+
+
+    }
+
+    public class Client
+    {
+        ConnectionSettings connectionSettings;
+        Socket socket;
+
+        public Client(ConnectionSettings settings)
+        {
+            connectionSettings = settings;
+        }
+
+    }
+
+    public class Server
+    {
+        ConnectionSettings connectionSettings;
+        Socket socket;
+
+        public Server(ConnectionSettings settings)
+        {
+            connectionSettings = settings;
+        }
+    }
+
+    public struct ConnectionSettings
+    {
+        public SocketType socketType;
+        public ConnectionType protocol;
+        public int port;
+    }
+
+    public enum SocketType
+    {
+        Default,
+        WebSocket
+    }
+
+    public enum ConnectionType
+    {
+        Udp,
+        Tcp
+    }
+}
